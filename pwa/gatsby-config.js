1264a16e12c62c3cb30da8230e2d227b04be75ea
ginokok1996 <ginokok1996@hotmail.com>
2021-11-03 17:06:39 +0100

    disabled csp for now

--- pwa/gatsby-config.js
+++ pwa/gatsby-config.js
@@ -10,18 +10,5 @@ module.exports = {
         pathToEmotionCacheProps: `src/emotion-cache-props`,
       },
     },
-    {
-      resolve: `gatsby-plugin-csp`,
-      options: {
-        disableOnDev: true,
-        mergeScriptHashes: true,
-        mergeStyleHashes: true,
-        directives: {
-          "script-src": "'self' 'unsafe-eval' https://unpkg.com/",
-          "style-src": "'self' 'nonce-true' https://unpkg.com/ 'unsafe-inline'",
-          "img-src": "'self' https://demodam.nl/"
-        }
-      }
-    }
   ]
 };
